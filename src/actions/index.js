1da35bd03e497ab251bcb48acbd861e376ceb99b
maq5ud <maksud@mudracircle.com>
2021-08-29 17:55:04 +0530

    feat : alternate method to overcome repeat request

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,17 @@ import _ from 'lodash';
 
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+
+    await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+
+    userIds.forEach(id => dispatch( fetchUser(id) ) );
+}
+
+
 export const fetchPosts = () => async (dispatch) => {
 
     const response = await jsonPlaceholder.get('/posts');
@@ -13,9 +24,7 @@ export const fetchPosts = () => async (dispatch) => {
 };
 
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
 
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
@@ -23,4 +32,14 @@ const _fetchUser = _.memoize(async (id, dispatch) => {
         type: 'FETCH_USER',
         payload: response.data,
     });
-});
+};
+
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data,
+//     });
+// });
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions'
+
 
 
 class UserHeader extends React.Component{ 
 
-    componentDidMount(){
-        this.props.fetchUser(this.props.userId);
-    }
+
 
     render(){
         
@@ -30,6 +28,5 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(
-    mapStateToProps,
-    { fetchUser },
+    mapStateToProps
 )(UserHeader);
